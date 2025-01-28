8c37f0403cdf133c64833774aa8cd06fbe82fa10
Azab <mostafahamdy9988@gmail.com>
2025-01-28 23:28:00 +0200

    add mdi-icon in main.js

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: "light",
+    //
+  },
 });
 const app = createApp(App);
 app.use(vuetify);
