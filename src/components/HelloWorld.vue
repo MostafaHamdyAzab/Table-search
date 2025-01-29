16fcd4081f8957ec6cc4a71227e949de464e1fdf
Azab <mostafahamdy9988@gmail.com>
2025-01-29 21:16:04 +0200

    change v-input border color

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,91 @@
 <template>
-  <v-card title="Nutrition" flat>
+  <v-card title="Test Mostafa Hamdy">
     <template v-slot:text>
-      <v-text-field
-        v-model="search"
-        label="Search"
-        prepend-inner-icon="mdi-magnify"
-        variant="outlined"
-        hide-details
-        single-line
-      ></v-text-field>
-    </template>
+      <v-container>
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+              v-model="search"
+              label="Search"
+              prepend-inner-icon="mdi-magnify"
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
 
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :search="search"
-    ></v-data-table>
+          <v-col cols="3">
+            <v-select
+              label="Camp"
+              :items="[
+                'California',
+                'Colorado',
+                'Florida',
+                'Georgia',
+                'Texas',
+                'Wyoming',
+              ]"
+              variant="outlined"
+              class="mr-1"
+            ></v-select>
+          </v-col>
+
+          <v-col cols="3">
+            <v-select
+              label="Tent"
+              :items="[
+                'California',
+                'Colorado',
+                'Florida',
+                'Georgia',
+                'Texas',
+                'Wyoming',
+              ]"
+              variant="outlined"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
+    <v-container>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :search="search"
+      ></v-data-table>
+    </v-container>
   </v-card>
 </template>
 
 <script>
 import data from "./data.json";
+
 export default {
   data() {
     return {
       search: "",
       headers: [
-        {
-          align: "start",
-          key: "hajjId",
-
-          title: "hajjId",
-        },
-        { key: "name", sortable: false, align: "center", title: "name" },
-        { key: "campId", title: "campId" },
-        { key: "passportNumber", title: "passportNumber" },
+        { align: "start", key: "hajjId", title: "ID" },
+        { key: "name", sortable: false, align: "center", title: "Name" },
+        { key: "campNumber", title: "Center (Camp)" },
+        { key: "tentNumber", title: "Tent" },
+        { key: "bedNumber", title: "Bed" },
       ],
       desserts: data,
     };
   },
 };
 </script>
+<style>
+thead {
+  background-color: #f6eeb4 !important;
+}
+
+:deep(.v-field__outline) {
+  border-color: red !important;
+}
+.v-field__outline {
+  border-color: red !important;
+}
+.v-input__control {
+  color: rgb(155, 32, 32) !important;
+}
+</style>
